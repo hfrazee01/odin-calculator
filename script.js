ee60924c15c6806298ec315448f6a1a29cc500a8
Harley Frazee <hfrazee01@gmail.com>
2023-08-06 11:49:41 -0400

    Replaced basic math functions with one operate function

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-const add = function(num1, num2) {
-    return num1 + num2;
-}
+let firstNumber;
+let secondNumber;
+let operator;
 
-const subtract = function(num1, num2) {
-    return num1 - num2;
-}
-
-const multiply = function(num1, num2) {
-    return num1 * num2;
-}
-
-const divide = function(num1, num2) {
-    if (num2 == 0) return "You can't divide by zero, silly goose!";
-    else return num1 / num2;
+const operate = function(num1, num2, op) {
+    if (op == "+") 
+        return num1 + num2;
+    else if (op == "-") 
+        return num1 - num2;
+    else if (op == "x") 
+        return num1 * num2;
+    else if (op == "/") {
+        if (num2 == 0) return "You can't divide by zero, silly goose!";
+        else return num1 / num2;
+    } else 
+        return "That's not an operation! Try again!";
 }
